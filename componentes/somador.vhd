495fb9cef19fa3894308f18f6ad9428701317afb
johnwsh <johnwillian0609@gmail.com>
2025-06-04 20:24:27 -0300

    coisa pra caralho

---
+++ componentes/somador.vhd
@@ -0,0 +1,18 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity somador is
+   port(
+        pcIn: in unsigned(15 downto 0);
+        pcOut: out unsigned(15 downto 0);
+   );
+end entity somador;
+
+architecture somador_a of somador is
+   
+begin
+
+    pcOut <= pcIn + 1;
+
+end architecture somador_a;
